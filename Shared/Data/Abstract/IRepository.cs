5a7c407e5c49cceac81fcbf7e3f75af751a9f7db
Tanju BOZOK <tnjbzk@gmail.com>
2024-11-17 00:25:53 +0300

    repository soyut ve somut sınıfları oluşturuldu.

---
+++ Shared/Data/Abstract/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Entities.Abstract;
+using System.Linq.Expressions;
+
+namespace Shared.Data.Abstract;
+
+public interface IRepository<T> where T : class, IEntity, new()
+{
+    Task<T?> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+    Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null, params Expression<Func<T, object>>[] includeProperties);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+    Task<int> CountAsync(Expression<Func<T, bool>> predicate);
+
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
---
+++ Shared/Data/Concrete/Repository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Data.Abstract;
+using Shared.Entities.Abstract;
+using System.Linq.Expressions;
+
+namespace Shared.Data.Concrete;
+
+public class Repository<T>(DbContext context) : IRepository<T> where T : class, IEntity, new()
+{
+    private readonly DbContext _context = context;
+
+    public async Task AddAsync(T entity)
+        => await _context.Set<T>().AddAsync(entity);
+
+    public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+        => await _context.Set<T>().AnyAsync(predicate);
+
+    public async Task<int> CountAsync(Expression<Func<T, bool>> predicate)
+        => await _context.Set<T>().CountAsync(predicate);
+
+    public async Task DeleteAsync(T entity)
+        => await Task.Run(() => { _context.Set<T>().Remove(entity); });
+
+    public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null, params Expression<Func<T, object>>[] includeProperties)
+    {
+        IQueryable<T> query = _context.Set<T>();
+
+        if (predicate != null)
+            query = query.Where(predicate);
+
+        query = includeProperties.Aggregate(query, (current, includeProperty)
+            => current.Include(includeProperty));
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<T?> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
+    {
+        IQueryable<T> query = _context.Set<T>();
+
+        if (predicate != null)
+            query = query.Where(predicate);
+
+        query = includeProperties.Aggregate(query, (current, includeProperty)
+            => current.Include(includeProperty));
+
+        return await query.FirstOrDefaultAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+        => await Task.Run(() => { _context.Set<T>().Update(entity); });
+}
